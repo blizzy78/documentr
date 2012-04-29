5fa074e1745fba44946851f33a1ca7a5b1600c72
Maik Schreiber <blizzy@blizzy.de>
2012-04-29 19:00:29 +0200

    refactoring

--- de.blizzy.documentr.test/src/de/blizzy/documentr/repository/PageStoreTest.java
+++ de.blizzy.documentr.test/src/de/blizzy/documentr/repository/PageStoreTest.java
@@ -5,6 +5,7 @@ import static org.mockito.Mockito.*;
 
 import java.io.File;
 import java.io.IOException;
+import java.util.Set;
 
 import org.eclipse.jgit.api.errors.GitAPIException;
 import org.junit.Before;
@@ -16,6 +17,12 @@ import de.blizzy.documentr.Settings;
 import de.blizzy.documentr.TestUtil;
 
 public class PageStoreTest {
+	private static final String PROJECT = "project"; //$NON-NLS-1$
+	private static final String BRANCH_1 = "branch_1"; //$NON-NLS-1$
+	private static final String BRANCH_2 = "branch_2"; //$NON-NLS-1$
+	private static final String BRANCH_3 = "branch_3"; //$NON-NLS-1$
+	private static final String PAGE = "page"; //$NON-NLS-1$
+	
 	private GlobalRepositoryManager globalRepoManager;
 	private PageStore pageStore;
 
@@ -38,90 +45,85 @@ public class PageStoreTest {
 	
 	@Test
 	public void isPageSharedWithOtherBranches() throws IOException, GitAPIException {
-		globalRepoManager.createProjectCentralRepository("project"); //$NON-NLS-1$
-		globalRepoManager.createProjectBranchRepository("project", "branch1", null); //$NON-NLS-1$ //$NON-NLS-2$
-		Page page = Page.fromText("title", "text"); //$NON-NLS-1$ //$NON-NLS-2$
-		pageStore.savePage("project", "branch1", "page1", page); //$NON-NLS-1$ //$NON-NLS-2$ //$NON-NLS-3$
+		globalRepoManager.createProjectCentralRepository(PROJECT);
+		globalRepoManager.createProjectBranchRepository(PROJECT, BRANCH_1, null);
+		saveRandomPage(BRANCH_1, PAGE);
 		
-		assertFalse(pageStore.isPageSharedWithOtherBranches("project", "branch1", "page1")); //$NON-NLS-1$ //$NON-NLS-2$ //$NON-NLS-3$
+		assertFalse(isPageSharedWithOtherBranches(BRANCH_1));
 
-		globalRepoManager.createProjectBranchRepository("project", "branch2", "branch1"); //$NON-NLS-1$ //$NON-NLS-2$ //$NON-NLS-3$
-		globalRepoManager.createProjectBranchRepository("project", "branch3", "branch1"); //$NON-NLS-1$ //$NON-NLS-2$ //$NON-NLS-3$
-		assertTrue(pageStore.isPageSharedWithOtherBranches("project", "branch1", "page1")); //$NON-NLS-1$ //$NON-NLS-2$ //$NON-NLS-3$
-		assertTrue(pageStore.isPageSharedWithOtherBranches("project", "branch2", "page1")); //$NON-NLS-1$ //$NON-NLS-2$ //$NON-NLS-3$
-		assertTrue(pageStore.isPageSharedWithOtherBranches("project", "branch3", "page1")); //$NON-NLS-1$ //$NON-NLS-2$ //$NON-NLS-3$
+		globalRepoManager.createProjectBranchRepository(PROJECT, BRANCH_2, BRANCH_1);
+		globalRepoManager.createProjectBranchRepository(PROJECT, BRANCH_3, BRANCH_1);
+		assertTrue(isPageSharedWithOtherBranches(BRANCH_1));
+		assertTrue(isPageSharedWithOtherBranches(BRANCH_2));
+		assertTrue(isPageSharedWithOtherBranches(BRANCH_3));
 		
-		page = Page.fromText("title", "text"); //$NON-NLS-1$ //$NON-NLS-2$
-		pageStore.savePage("project", "branch1", "page2", page); //$NON-NLS-1$ //$NON-NLS-2$ //$NON-NLS-3$
-		page = Page.fromText("title", "text"); //$NON-NLS-1$ //$NON-NLS-2$
-		pageStore.savePage("project", "branch2", "page3", page); //$NON-NLS-1$ //$NON-NLS-2$ //$NON-NLS-3$
-		page = Page.fromText("title", "text"); //$NON-NLS-1$ //$NON-NLS-2$
-		pageStore.savePage("project", "branch3", "page4", page); //$NON-NLS-1$ //$NON-NLS-2$ //$NON-NLS-3$
-		assertTrue(pageStore.isPageSharedWithOtherBranches("project", "branch1", "page1")); //$NON-NLS-1$ //$NON-NLS-2$ //$NON-NLS-3$
-		assertTrue(pageStore.isPageSharedWithOtherBranches("project", "branch2", "page1")); //$NON-NLS-1$ //$NON-NLS-2$ //$NON-NLS-3$
-		assertTrue(pageStore.isPageSharedWithOtherBranches("project", "branch3", "page1")); //$NON-NLS-1$ //$NON-NLS-2$ //$NON-NLS-3$
+		saveRandomPage(BRANCH_1, "page_2"); //$NON-NLS-1$
+		saveRandomPage(BRANCH_2, "page_3"); //$NON-NLS-1$
+		saveRandomPage(BRANCH_3, "page_4"); //$NON-NLS-1$
+		assertTrue(isPageSharedWithOtherBranches(BRANCH_1));
+		assertTrue(isPageSharedWithOtherBranches(BRANCH_2));
+		assertTrue(isPageSharedWithOtherBranches(BRANCH_3));
 
-		page = Page.fromText("title", "text 2"); //$NON-NLS-1$ //$NON-NLS-2$
-		pageStore.savePage("project", "branch1", "page1", page); //$NON-NLS-1$ //$NON-NLS-2$ //$NON-NLS-3$
-		assertFalse(pageStore.isPageSharedWithOtherBranches("project", "branch1", "page1")); //$NON-NLS-1$ //$NON-NLS-2$ //$NON-NLS-3$
-		assertTrue(pageStore.isPageSharedWithOtherBranches("project", "branch2", "page1")); //$NON-NLS-1$ //$NON-NLS-2$ //$NON-NLS-3$
-		assertTrue(pageStore.isPageSharedWithOtherBranches("project", "branch3", "page1")); //$NON-NLS-1$ //$NON-NLS-2$ //$NON-NLS-3$
+		saveRandomPage(BRANCH_1, PAGE);
+		assertFalse(isPageSharedWithOtherBranches(BRANCH_1));
+		assertTrue(isPageSharedWithOtherBranches(BRANCH_2));
+		assertTrue(isPageSharedWithOtherBranches(BRANCH_3));
 		
-		page = Page.fromText("title", "text 3"); //$NON-NLS-1$ //$NON-NLS-2$
-		pageStore.savePage("project", "branch2", "page1", page); //$NON-NLS-1$ //$NON-NLS-2$ //$NON-NLS-3$
-		assertFalse(pageStore.isPageSharedWithOtherBranches("project", "branch1", "page1")); //$NON-NLS-1$ //$NON-NLS-2$ //$NON-NLS-3$
-		assertFalse(pageStore.isPageSharedWithOtherBranches("project", "branch2", "page1")); //$NON-NLS-1$ //$NON-NLS-2$ //$NON-NLS-3$
-		assertFalse(pageStore.isPageSharedWithOtherBranches("project", "branch3", "page1")); //$NON-NLS-1$ //$NON-NLS-2$ //$NON-NLS-3$
+		saveRandomPage(BRANCH_2, PAGE);
+		assertFalse(isPageSharedWithOtherBranches(BRANCH_1));
+		assertFalse(isPageSharedWithOtherBranches(BRANCH_2));
+		assertFalse(isPageSharedWithOtherBranches(BRANCH_3));
+	}
+	
+	private boolean isPageSharedWithOtherBranches(String branchName) throws IOException {
+		return pageStore.isPageSharedWithOtherBranches(PROJECT, branchName, PAGE);
 	}
 	
 	@Test
 	public void getBranchesPageIsSharedWith() throws IOException, GitAPIException {
-		globalRepoManager.createProjectCentralRepository("project"); //$NON-NLS-1$
-		globalRepoManager.createProjectBranchRepository("project", "branch1", null); //$NON-NLS-1$ //$NON-NLS-2$
-		Page page = Page.fromText("title", "text"); //$NON-NLS-1$ //$NON-NLS-2$
-		pageStore.savePage("project", "branch1", "page1", page); //$NON-NLS-1$ //$NON-NLS-2$ //$NON-NLS-3$
+		globalRepoManager.createProjectCentralRepository(PROJECT);
+		globalRepoManager.createProjectBranchRepository(PROJECT, BRANCH_1, null);
+		saveRandomPage(BRANCH_1, PAGE);
 
-		assertEquals(Sets.newHashSet("branch1"), //$NON-NLS-1$
-				pageStore.getBranchesPageIsSharedWith("project", "branch1", "page1")); //$NON-NLS-1$ //$NON-NLS-2$ //$NON-NLS-3$
+		assertBranchesPageIsSharedWith(BRANCH_1, BRANCH_1);
 		
-		globalRepoManager.createProjectBranchRepository("project", "branch2", "branch1"); //$NON-NLS-1$ //$NON-NLS-2$ //$NON-NLS-3$
-		globalRepoManager.createProjectBranchRepository("project", "branch3", "branch1"); //$NON-NLS-1$ //$NON-NLS-2$ //$NON-NLS-3$
-		assertEquals(Sets.newHashSet("branch1", "branch2", "branch3"), //$NON-NLS-1$ //$NON-NLS-2$ //$NON-NLS-3$
-				pageStore.getBranchesPageIsSharedWith("project", "branch1", "page1")); //$NON-NLS-1$ //$NON-NLS-2$ //$NON-NLS-3$
-		assertEquals(Sets.newHashSet("branch1", "branch2", "branch3"), //$NON-NLS-1$ //$NON-NLS-2$ //$NON-NLS-3$
-				pageStore.getBranchesPageIsSharedWith("project", "branch2", "page1")); //$NON-NLS-1$ //$NON-NLS-2$ //$NON-NLS-3$
-		assertEquals(Sets.newHashSet("branch1", "branch2", "branch3"), //$NON-NLS-1$ //$NON-NLS-2$ //$NON-NLS-3$
-				pageStore.getBranchesPageIsSharedWith("project", "branch3", "page1")); //$NON-NLS-1$ //$NON-NLS-2$ //$NON-NLS-3$
+		globalRepoManager.createProjectBranchRepository(PROJECT, BRANCH_2, BRANCH_1);
+		globalRepoManager.createProjectBranchRepository(PROJECT, BRANCH_3, BRANCH_1);
+		assertBranchesPageIsSharedWith(BRANCH_1, BRANCH_1, BRANCH_2, BRANCH_3);
+		assertBranchesPageIsSharedWith(BRANCH_2, BRANCH_1, BRANCH_2, BRANCH_3);
+		assertBranchesPageIsSharedWith(BRANCH_3, BRANCH_1, BRANCH_2, BRANCH_3);
 		
-		page = Page.fromText("title", "text"); //$NON-NLS-1$ //$NON-NLS-2$
-		pageStore.savePage("project", "branch1", "page2", page); //$NON-NLS-1$ //$NON-NLS-2$ //$NON-NLS-3$
-		page = Page.fromText("title", "text"); //$NON-NLS-1$ //$NON-NLS-2$
-		pageStore.savePage("project", "branch2", "page3", page); //$NON-NLS-1$ //$NON-NLS-2$ //$NON-NLS-3$
-		page = Page.fromText("title", "text"); //$NON-NLS-1$ //$NON-NLS-2$
-		pageStore.savePage("project", "branch3", "page4", page); //$NON-NLS-1$ //$NON-NLS-2$ //$NON-NLS-3$
-		assertEquals(Sets.newHashSet("branch1", "branch2", "branch3"), //$NON-NLS-1$ //$NON-NLS-2$ //$NON-NLS-3$
-				pageStore.getBranchesPageIsSharedWith("project", "branch1", "page1")); //$NON-NLS-1$ //$NON-NLS-2$ //$NON-NLS-3$
-		assertEquals(Sets.newHashSet("branch1", "branch2", "branch3"), //$NON-NLS-1$ //$NON-NLS-2$ //$NON-NLS-3$
-				pageStore.getBranchesPageIsSharedWith("project", "branch2", "page1")); //$NON-NLS-1$ //$NON-NLS-2$ //$NON-NLS-3$
-		assertEquals(Sets.newHashSet("branch1", "branch2", "branch3"), //$NON-NLS-1$ //$NON-NLS-2$ //$NON-NLS-3$
-				pageStore.getBranchesPageIsSharedWith("project", "branch3", "page1")); //$NON-NLS-1$ //$NON-NLS-2$ //$NON-NLS-3$
+		saveRandomPage(BRANCH_1, "page_2"); //$NON-NLS-1$
+		saveRandomPage(BRANCH_2, "page_3"); //$NON-NLS-1$
+		saveRandomPage(BRANCH_3, "page_4"); //$NON-NLS-1$
+		assertBranchesPageIsSharedWith(BRANCH_1, BRANCH_1, BRANCH_2, BRANCH_3);
+		assertBranchesPageIsSharedWith(BRANCH_2, BRANCH_1, BRANCH_2, BRANCH_3);
+		assertBranchesPageIsSharedWith(BRANCH_3, BRANCH_1, BRANCH_2, BRANCH_3);
 		
-		page = Page.fromText("title", "text 2"); //$NON-NLS-1$ //$NON-NLS-2$
-		pageStore.savePage("project", "branch1", "page1", page); //$NON-NLS-1$ //$NON-NLS-2$ //$NON-NLS-3$
-		assertEquals(Sets.newHashSet("branch1"), //$NON-NLS-1$
-				pageStore.getBranchesPageIsSharedWith("project", "branch1", "page1")); //$NON-NLS-1$ //$NON-NLS-2$ //$NON-NLS-3$
-		assertEquals(Sets.newHashSet("branch2", "branch3"), //$NON-NLS-1$ //$NON-NLS-2$
-				pageStore.getBranchesPageIsSharedWith("project", "branch2", "page1")); //$NON-NLS-1$ //$NON-NLS-2$ //$NON-NLS-3$
-		assertEquals(Sets.newHashSet("branch2", "branch3"), //$NON-NLS-1$ //$NON-NLS-2$
-				pageStore.getBranchesPageIsSharedWith("project", "branch3", "page1")); //$NON-NLS-1$ //$NON-NLS-2$ //$NON-NLS-3$
+		saveRandomPage(BRANCH_1, PAGE);
+		assertBranchesPageIsSharedWith(BRANCH_1, BRANCH_1);
+		assertBranchesPageIsSharedWith(BRANCH_2, BRANCH_2, BRANCH_3);
+		assertBranchesPageIsSharedWith(BRANCH_3, BRANCH_2, BRANCH_3);
 		
-		page = Page.fromText("title", "text 3"); //$NON-NLS-1$ //$NON-NLS-2$
-		pageStore.savePage("project", "branch2", "page1", page); //$NON-NLS-1$ //$NON-NLS-2$ //$NON-NLS-3$
-		assertEquals(Sets.newHashSet("branch1"), //$NON-NLS-1$
-				pageStore.getBranchesPageIsSharedWith("project", "branch1", "page1")); //$NON-NLS-1$ //$NON-NLS-2$ //$NON-NLS-3$
-		assertEquals(Sets.newHashSet("branch2"), //$NON-NLS-1$
-				pageStore.getBranchesPageIsSharedWith("project", "branch2", "page1")); //$NON-NLS-1$ //$NON-NLS-2$ //$NON-NLS-3$
-		assertEquals(Sets.newHashSet("branch3"), //$NON-NLS-1$
-				pageStore.getBranchesPageIsSharedWith("project", "branch3", "page1")); //$NON-NLS-1$ //$NON-NLS-2$ //$NON-NLS-3$
+		saveRandomPage(BRANCH_2, PAGE);
+		assertBranchesPageIsSharedWith(BRANCH_1, BRANCH_1);
+		assertBranchesPageIsSharedWith(BRANCH_2, BRANCH_2);
+		assertBranchesPageIsSharedWith(BRANCH_3, BRANCH_3);
+	}
+
+	private void assertBranchesPageIsSharedWith(String branchName, String... expectedBranches)
+			throws IOException {
+		
+		Set<String> branches = pageStore.getBranchesPageIsSharedWith(PROJECT, branchName, PAGE);
+		assertEquals(Sets.newHashSet(expectedBranches), branches);
+	}
+	
+	private void saveRandomPage(String branchName, String path) throws IOException, GitAPIException {
+		Page page = createRandomPage();
+		pageStore.savePage(PROJECT, branchName, path, page);
+	}
+	
+	private Page createRandomPage() {
+		return Page.fromText(String.valueOf(Math.random() * Long.MAX_VALUE), String.valueOf(Math.random() * Long.MAX_VALUE));
 	}
 }
