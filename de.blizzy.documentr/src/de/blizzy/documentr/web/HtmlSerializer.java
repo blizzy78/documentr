88665a0a21c7fcba5100460890d5fd32d12b13ad
Maik Schreiber <blizzy@blizzy.de>
2012-05-01 16:48:01 +0200

    show pretty tables

--- de.blizzy.documentr/src/de/blizzy/documentr/web/HtmlSerializer.java
+++ de.blizzy.documentr/src/de/blizzy/documentr/web/HtmlSerializer.java
@@ -2,6 +2,7 @@ package de.blizzy.documentr.web;
 
 import org.pegdown.LinkRenderer;
 import org.pegdown.ToHtmlSerializer;
+import org.pegdown.ast.SuperNode;
 import org.pegdown.ast.VerbatimNode;
 
 public class HtmlSerializer extends ToHtmlSerializer {
@@ -19,5 +20,16 @@ public class HtmlSerializer extends ToHtmlSerializer {
 		}
 		printer.printEncoded(text);
 		printer.print("</code></pre>"); //$NON-NLS-1$
+	}
+	
+	@Override
+	protected void printIndentedTag(SuperNode node, String tag) {
+		if (tag.equals("table")) { //$NON-NLS-1$
+			printer.println().print("<table class=\"table-documentr table-bordered table-striped table-condensed\">").indent(2); //$NON-NLS-1$
+			visitChildren(node);
+			printer.indent(-2).println().print("</table>"); //$NON-NLS-1$
+		} else {
+			super.printIndentedTag(node, tag);
+		}
 	}
 }
