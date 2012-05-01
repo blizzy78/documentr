13a43560cb42553ce8948ef53f18f01fe41974b1
Maik Schreiber <blizzy@blizzy.de>
2012-05-01 18:53:10 +0200

    reject dots in project names and page paths

--- de.blizzy.documentr/src/de/blizzy/documentr/DocumentrConstants.java
+++ de.blizzy.documentr/src/de/blizzy/documentr/DocumentrConstants.java
@@ -3,9 +3,9 @@ package de.blizzy.documentr;
 import org.eclipse.jgit.lib.Constants;
 
 public final class DocumentrConstants {
-	public static final String PROJECT_NAME_PATTERN = "[a-zA-Z0-9_\\.\\-]+"; //$NON-NLS-1$
+	public static final String PROJECT_NAME_PATTERN = "[a-zA-Z0-9_\\-]+"; //$NON-NLS-1$
 	public static final String BRANCH_NAME_PATTERN = "[a-zA-Z0-9_\\.\\-]+"; //$NON-NLS-1$
-	private static final String PAGE_NAME_VALID_CHARS_PATTERN = "[a-zA-Z0-9_\\.\\-]+"; //$NON-NLS-1$
+	private static final String PAGE_NAME_VALID_CHARS_PATTERN = "[a-zA-Z0-9_\\-]+"; //$NON-NLS-1$
 	private static final String PAGE_PATH_REAL_PATTERN =
 			PAGE_NAME_VALID_CHARS_PATTERN + "(?:/" + PAGE_NAME_VALID_CHARS_PATTERN + ")*"; //$NON-NLS-1$ //$NON-NLS-2$
 	public static final String PAGE_PATH_URL_PATTERN =
@@ -16,7 +16,7 @@ public final class DocumentrConstants {
 	public static final String PROJECT_NAMES_BLACKLIST_PATTERN = "(?:create|save|_.*)"; //$NON-NLS-1$
 	public static final String BRANCH_NAMES_BLACKLIST_PATTERN =
 			"(?:create|save|" + Constants.MASTER + "|_.*)"; //$NON-NLS-1$ //$NON-NLS-2$
-	public static final String PAGE_PATHS_BLACKLIST_PATTERN = "([_\\.].*|.*/[_\\.].*)"; //$NON-NLS-1$
+	public static final String PAGE_PATHS_BLACKLIST_PATTERN = "(_.*|.*/_.*)"; //$NON-NLS-1$
 	
 	private DocumentrConstants() {}
 }
