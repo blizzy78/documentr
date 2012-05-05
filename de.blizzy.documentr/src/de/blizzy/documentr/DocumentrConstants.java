55219693733c919e1dc99a27cbdb56a9b127bac0
Maik Schreiber <blizzy@blizzy.de>
2012-05-05 10:46:40 +0200

    reject branch names starting with '.'

--- de.blizzy.documentr/src/de/blizzy/documentr/DocumentrConstants.java
+++ de.blizzy.documentr/src/de/blizzy/documentr/DocumentrConstants.java
@@ -13,9 +13,9 @@ public final class DocumentrConstants {
 	public static final String PAGE_PATH_PATTERN =
 			"(?:" + PAGE_PATH_REAL_PATTERN + "|" + PAGE_PATH_URL_PATTERN + ")"; //$NON-NLS-1$ //$NON-NLS-2$ //$NON-NLS-3$
 
-	public static final String PROJECT_NAMES_BLACKLIST_PATTERN = "(?:create|save|_.*)"; //$NON-NLS-1$
+	public static final String PROJECT_NAMES_BLACKLIST_PATTERN = "(?:create|save|list|_.*)"; //$NON-NLS-1$
 	public static final String BRANCH_NAMES_BLACKLIST_PATTERN =
-			"(?:create|save|" + Constants.MASTER + "|_.*)"; //$NON-NLS-1$ //$NON-NLS-2$
+			"(?:create|save|" + Constants.MASTER + "|\\..*|_.*)"; //$NON-NLS-1$ //$NON-NLS-2$
 	public static final String PAGE_PATHS_BLACKLIST_PATTERN = "(_.*|.*/_.*)"; //$NON-NLS-1$
 	
 	private DocumentrConstants() {}
