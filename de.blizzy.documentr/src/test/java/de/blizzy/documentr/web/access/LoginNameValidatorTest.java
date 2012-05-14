e1245d7cbaba29f344bff19f82942cd00c76a173
Maik Schreiber <blizzy@blizzy.de>
2012-05-14 16:25:34 +0200

    add more unit tests

--- de.blizzy.documentr/src/test/java/de/blizzy/documentr/web/access/LoginNameValidatorTest.java
+++ de.blizzy.documentr/src/test/java/de/blizzy/documentr/web/access/LoginNameValidatorTest.java
@@ -19,12 +19,15 @@ package de.blizzy.documentr.web.access;
 
 import static org.junit.Assert.*;
 
+import org.apache.commons.lang3.StringUtils;
 import org.junit.Test;
 
 public class LoginNameValidatorTest {
 	@Test
 	public void isValid() {
 		LoginNameValidator validator = new LoginNameValidator();
+		assertTrue(validator.isValid(null, null));
+		assertTrue(validator.isValid(StringUtils.EMPTY, null));
 		assertTrue(validator.isValid("admin", null)); //$NON-NLS-1$
 		assertFalse(validator.isValid("x/y", null)); //$NON-NLS-1$
 		assertFalse(validator.isValid("x,y", null)); //$NON-NLS-1$
