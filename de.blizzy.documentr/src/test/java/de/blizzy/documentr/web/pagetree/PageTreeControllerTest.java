7bd74999e6154be3c27bbc49dad0e410f6f6808a
Maik Schreiber <blizzy@blizzy.de>
2012-06-18 02:03:12 +0200

    add more unit tests

---
+++ de.blizzy.documentr/src/test/java/de/blizzy/documentr/web/pagetree/PageTreeControllerTest.java
@@ -0,0 +1,47 @@
+/*
+documentr - Edit, maintain, and present software documentation on the web.
+Copyright (C) 2012 Maik Schreiber
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+package de.blizzy.documentr.web.pagetree;
+
+import org.junit.Ignore;
+import org.junit.Test;
+
+public class PageTreeControllerTest {
+	@Test
+	@Ignore
+	public void getApplicationChildren() {
+		// TODO: implement test
+	}
+
+	@Test
+	@Ignore
+	public void getProjectChildren() {
+		// TODO: implement test
+	}
+	
+	@Test
+	@Ignore
+	public void getBranchChildren() {
+		// TODO: implement test
+	}
+	
+	@Test
+	@Ignore
+	public void getPageChildren() {
+		// TODO: implement test
+	}
+}
