8da007f418f0f7a492bda95a80c8b141830496e6
Maik Schreiber <blizzy@blizzy.de>
2012-07-07 14:00:08 +0200

    add more unit tests

---
+++ de.blizzy.documentr/src/test/java/de/blizzy/documentr/web/search/SearchControllerTest.java
@@ -0,0 +1,59 @@
+/*
+documentr - Edit, maintain, and present software documentation on the web.
+Copyright (C) 2012 Maik Schreiber
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+package de.blizzy.documentr.web.search;
+
+import static org.junit.Assert.*;
+import static org.mockito.Mockito.*;
+
+import java.io.IOException;
+
+import org.apache.lucene.queryParser.ParseException;
+import org.junit.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.springframework.security.core.Authentication;
+import org.springframework.ui.Model;
+
+import de.blizzy.documentr.AbstractDocumentrTest;
+import de.blizzy.documentr.search.PageIndex;
+import de.blizzy.documentr.search.SearchResult;
+
+public class SearchControllerTest extends AbstractDocumentrTest {
+	@InjectMocks
+	private SearchController searchController;
+	@Mock
+	private PageIndex pageIndex;
+	@Mock
+	private Authentication authentication;
+	@Mock
+	private Model model;
+	@Mock
+	private SearchResult searchResult;
+	
+	@Test
+	public void findPages() throws IOException, ParseException {
+		when(pageIndex.findPages("text", 1, authentication)).thenReturn(searchResult); //$NON-NLS-1$
+		
+		String view = searchController.findPages("text", Integer.valueOf(1), authentication, model); //$NON-NLS-1$
+		assertEquals("/search/result", view); //$NON-NLS-1$
+		
+		verify(model).addAttribute("searchText", "text"); //$NON-NLS-1$ //$NON-NLS-2$
+		verify(model).addAttribute("searchResult", searchResult); //$NON-NLS-1$
+		verify(model).addAttribute("page", Integer.valueOf(1)); //$NON-NLS-1$
+	}
+}
