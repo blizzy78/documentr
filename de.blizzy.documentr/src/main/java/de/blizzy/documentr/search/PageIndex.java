3effc2206be0720bcf384b0565a4dda773c0329d
Maik Schreiber <blizzy@blizzy.de>
2012-07-08 03:01:51 +0200

    reuse IndexWriter and IndexSearcher as much as possible

--- de.blizzy.documentr/src/main/java/de/blizzy/documentr/search/PageIndex.java
+++ de.blizzy.documentr/src/main/java/de/blizzy/documentr/search/PageIndex.java
@@ -112,12 +112,21 @@ public class PageIndex {
 	private Analyzer analyzer = new EnglishAnalyzer(Version.LUCENE_36);
 	private File pageIndexDir;
 	private ExecutorService threadPool = Executors.newFixedThreadPool(1);
+	private Directory directory;
+	private IndexWriter writer;
+	private IndexSearcher searcher;
 	
 	@PostConstruct
 	public void init() throws IOException {
 		File indexDir = new File(settings.getDocumentrDataDir(), "index"); //$NON-NLS-1$
 		pageIndexDir = new File(indexDir, "page"); //$NON-NLS-1$
 		FileUtils.forceMkdir(pageIndexDir);
+		
+		directory = FSDirectory.open(pageIndexDir);
+		
+		IndexWriterConfig config = new IndexWriterConfig(Version.LUCENE_36, analyzer);
+		config.setOpenMode(OpenMode.CREATE_OR_APPEND);
+		writer = new IndexWriter(directory, config);
 	}
 	
 	@PreDestroy
@@ -128,9 +137,18 @@ public class PageIndex {
 		} catch (InterruptedException e) {
 			// ignore
 		}
+		IndexUtil.closeQuietly(searcher);
+		IndexUtil.closeQuietly(writer);
+		IndexUtil.closeQuietly(directory);
 	}
 	
 	public void addPage(final String projectName, final String branchName, final String path, final Page page) {
+		Assert.hasLength(projectName);
+		Assert.hasLength(branchName);
+		Assert.hasLength(path);
+		Assert.notNull(page);
+		Assert.isTrue(page.getData() instanceof PageTextData);
+
 		Runnable runnable = new Runnable() {
 			@Override
 			public void run() {
@@ -146,41 +164,24 @@ public class PageIndex {
 		threadPool.submit(runnable);
 	}
 	
-	private synchronized void addPageInternal(String projectName, String branchName, String path, Page page)
-			throws IOException {
-		
-		Assert.hasLength(projectName);
-		Assert.hasLength(branchName);
-		Assert.hasLength(path);
-		Assert.notNull(page);
-		Assert.isTrue(page.getData() instanceof PageTextData);
+	private void addPageInternal(String projectName, String branchName, String path, Page page) throws IOException {
+		String fullPath = projectName + "/" + branchName + "/" + Util.toURLPagePath(path); //$NON-NLS-1$ //$NON-NLS-2$
+		String text = ((PageTextData) page.getData()).getText();
+		Authentication authentication = authenticationFactory.create("dummy"); //$NON-NLS-1$
+		text = markdownProcessor.markdownToHTML(text, projectName, branchName, path, authentication, false);
+		text = removeHtmlTags(text);
+		text = replaceHtmlEntities(text);
 
-		Directory directory = null;
-		IndexWriter writer = null;
-		try {
-			directory = FSDirectory.open(pageIndexDir);
-			IndexWriterConfig config = new IndexWriterConfig(Version.LUCENE_36, analyzer);
-			config.setOpenMode(OpenMode.CREATE_OR_APPEND);
-			writer = new IndexWriter(directory, config);
-	
-			Document doc = new Document();
-			String fullPath = projectName + "/" + branchName + "/" + Util.toURLPagePath(path); //$NON-NLS-1$ //$NON-NLS-2$
-			doc.add(new Field("fullPath", fullPath, Store.NO, Index.NOT_ANALYZED, TermVector.NO)); //$NON-NLS-1$
-			doc.add(new Field("project", projectName, Store.YES, Index.NOT_ANALYZED, TermVector.NO)); //$NON-NLS-1$
-			doc.add(new Field("branch", branchName, Store.YES, Index.NOT_ANALYZED, TermVector.NO)); //$NON-NLS-1$
-			doc.add(new Field("path", path, Store.YES, Index.NOT_ANALYZED, TermVector.NO)); //$NON-NLS-1$
-			doc.add(new Field("title", page.getTitle(), Store.YES, Index.ANALYZED, TermVector.YES)); //$NON-NLS-1$
-			String text = ((PageTextData) page.getData()).getText();
-			Authentication authentication = authenticationFactory.create("dummy"); //$NON-NLS-1$
-			text = markdownProcessor.markdownToHTML(text, projectName, branchName, path, authentication, false);
-			text = removeHtmlTags(text);
-			text = replaceHtmlEntities(text);
-			doc.add(new Field("text", text, Store.YES, Index.ANALYZED, TermVector.YES)); //$NON-NLS-1$
-			writer.updateDocument(new Term("fullPath", fullPath), doc); //$NON-NLS-1$
-		} finally {
-			IndexUtil.closeQuietly(writer);
-			IndexUtil.closeQuietly(directory);
-		}
+		Document doc = new Document();
+		doc.add(new Field("fullPath", fullPath, Store.NO, Index.NOT_ANALYZED, TermVector.NO)); //$NON-NLS-1$
+		doc.add(new Field("project", projectName, Store.YES, Index.NOT_ANALYZED, TermVector.NO)); //$NON-NLS-1$
+		doc.add(new Field("branch", branchName, Store.YES, Index.NOT_ANALYZED, TermVector.NO)); //$NON-NLS-1$
+		doc.add(new Field("path", path, Store.YES, Index.NOT_ANALYZED, TermVector.NO)); //$NON-NLS-1$
+		doc.add(new Field("title", page.getTitle(), Store.YES, Index.ANALYZED, TermVector.YES)); //$NON-NLS-1$
+		doc.add(new Field("text", text, Store.YES, Index.ANALYZED, TermVector.YES)); //$NON-NLS-1$
+		writer.updateDocument(new Term("fullPath", fullPath), doc); //$NON-NLS-1$
+
+		writer.commit();
 	}
 	
 	private String removeHtmlTags(String html) {
@@ -213,6 +214,10 @@ public class PageIndex {
 	}
 
 	public void deletePages(final String projectName, final String branchName, final Set<String> paths) {
+		Assert.hasLength(projectName);
+		Assert.hasLength(branchName);
+		Assert.notEmpty(paths);
+
 		Runnable runnable = new Runnable() {
 			@Override
 			public void run() {
@@ -226,33 +231,25 @@ public class PageIndex {
 		threadPool.submit(runnable);
 	}
 
-	private synchronized void deletePagesInternal(String projectName, String branchName, Set<String> paths)
-			throws IOException {
-		
-		Assert.hasLength(projectName);
-		Assert.hasLength(branchName);
-		Assert.notEmpty(paths);
-
-		Directory directory = null;
-		IndexWriter writer = null;
+	private void deletePagesInternal(String projectName, String branchName, Set<String> paths) throws IOException {
+		boolean dirty = false;
 		try {
-			directory = FSDirectory.open(pageIndexDir);
-			IndexWriterConfig config = new IndexWriterConfig(Version.LUCENE_36, analyzer);
-			config.setOpenMode(OpenMode.CREATE_OR_APPEND);
-			writer = new IndexWriter(directory, config);
-
 			for (String path : paths) {
 				String fullPath = projectName + "/" + branchName + "/" + Util.toURLPagePath(path); //$NON-NLS-1$ //$NON-NLS-2$
 				writer.deleteDocuments(new Term("fullPath", fullPath)); //$NON-NLS-1$
+				dirty = true;
 			}
 		} finally {
-			IndexUtil.closeQuietly(writer);
-			IndexUtil.closeQuietly(directory);
+			if (dirty) {
+				writer.commit();
+			}
 		}
 	}
 
 	public SearchResult findPages(String searchText, int page, Authentication authentication) throws ParseException, IOException {
+		Assert.hasLength(searchText);
 		Assert.isTrue(page >= 1);
+		Assert.notNull(authentication);
 		
 		QueryParser titleParser = new QueryParser(Version.LUCENE_36, "title", analyzer); //$NON-NLS-1$
 		Query titleQuery = titleParser.parse(searchText);
@@ -263,54 +260,59 @@ public class PageIndex {
 		query.add(titleQuery, Occur.SHOULD);
 		query.add(textQuery, Occur.SHOULD);
 		
-		Directory directory = null;
-		IndexReader reader = null;
-		IndexSearcher searcher = null;
 		List<SearchHit> hits = Lists.newArrayList();
-		int totalHits;
-		try {
-			directory = FSDirectory.open(pageIndexDir);
-			reader = IndexReader.open(directory);
-			searcher = new IndexSearcher(reader);
-			Filter filter = new PagePermissionFilter(authentication, Permission.VIEW, permissionEvaluator);
-			TopDocs docs = searcher.search(query, filter, HITS_PER_PAGE * page);
-			totalHits = docs.totalHits;
-			
-			Formatter formatter = new SimpleHTMLFormatter("<strong>", "</strong>"); //$NON-NLS-1$ //$NON-NLS-2$
-			Scorer scorer = new QueryScorer(query);
-			Highlighter highlighter = new Highlighter(formatter, scorer);
-			highlighter.setTextFragmenter(new SimpleFragmenter(FRAGMENT_SIZE));
-			highlighter.setEncoder(new SimpleHTMLEncoder());
-			int start = HITS_PER_PAGE * (page - 1);
-			int end = Math.min(HITS_PER_PAGE * page, docs.scoreDocs.length);
-			for (int i = start; i < end; i++) {
-				int docId = docs.scoreDocs[i].doc;
-				Document doc = reader.document(docId);
-				String projectName = doc.get("project"); //$NON-NLS-1$
-				String branchName = doc.get("branch"); //$NON-NLS-1$
-				String path = doc.get("path"); //$NON-NLS-1$
-				String title = doc.get("title"); //$NON-NLS-1$
-				String text = doc.get("text"); //$NON-NLS-1$
-				TokenStream tokenStream = null;
-				try {
-					tokenStream = TokenSources.getAnyTokenStream(reader, docId, "text", doc, analyzer); //$NON-NLS-1$
-					String[] fragments = highlighter.getBestFragments(tokenStream, text, NUM_FRAGMENTS);
-					cleanupFragments(fragments);
-					String highlightedText = Util.join(fragments, " <strong>...</strong> "); //$NON-NLS-1$
-					SearchHit hit = new SearchHit(projectName, branchName, path, title, highlightedText);
-					hits.add(hit);
-				} catch (InvalidTokenOffsetsException e) {
-					// ignore
-				} finally {
-					IndexUtil.closeQuietly(tokenStream);
-				}
+		IndexSearcher searcher = getSearcher();
+		IndexReader reader = searcher.getIndexReader();
+		Filter filter = new PagePermissionFilter(authentication, Permission.VIEW, permissionEvaluator);
+		TopDocs docs = searcher.search(query, filter, HITS_PER_PAGE * page);
+		Formatter formatter = new SimpleHTMLFormatter("<strong>", "</strong>"); //$NON-NLS-1$ //$NON-NLS-2$
+		Scorer scorer = new QueryScorer(query);
+		Highlighter highlighter = new Highlighter(formatter, scorer);
+		highlighter.setTextFragmenter(new SimpleFragmenter(FRAGMENT_SIZE));
+		highlighter.setEncoder(new SimpleHTMLEncoder());
+		int start = HITS_PER_PAGE * (page - 1);
+		int end = Math.min(HITS_PER_PAGE * page, docs.scoreDocs.length);
+		for (int i = start; i < end; i++) {
+			int docId = docs.scoreDocs[i].doc;
+			Document doc = reader.document(docId);
+			String projectName = doc.get("project"); //$NON-NLS-1$
+			String branchName = doc.get("branch"); //$NON-NLS-1$
+			String path = doc.get("path"); //$NON-NLS-1$
+			String title = doc.get("title"); //$NON-NLS-1$
+			String text = doc.get("text"); //$NON-NLS-1$
+			TokenStream tokenStream = null;
+			try {
+				tokenStream = TokenSources.getAnyTokenStream(reader, docId, "text", doc, analyzer); //$NON-NLS-1$
+				String[] fragments = highlighter.getBestFragments(tokenStream, text, NUM_FRAGMENTS);
+				cleanupFragments(fragments);
+				String highlightedText = Util.join(fragments, " <strong>...</strong> "); //$NON-NLS-1$
+				SearchHit hit = new SearchHit(projectName, branchName, path, title, highlightedText);
+				hits.add(hit);
+			} catch (InvalidTokenOffsetsException e) {
+				// ignore
+			} finally {
+				IndexUtil.closeQuietly(tokenStream);
 			}
-		} finally {
-			IndexUtil.closeQuietly(searcher);
-			IndexUtil.closeQuietly(reader);
-			IndexUtil.closeQuietly(directory);
 		}
-		return new SearchResult(hits, totalHits, HITS_PER_PAGE);
+		return new SearchResult(hits, docs.totalHits, HITS_PER_PAGE);
+	}
+
+	private synchronized IndexSearcher getSearcher() throws IOException {
+		if (searcher != null) {
+			IndexReader reader = searcher.getIndexReader();
+			if (!reader.isCurrent()) {
+				IndexUtil.closeQuietly(searcher);
+				IndexUtil.closeQuietly(reader);
+				searcher = null;
+			}
+		}
+		
+		if (searcher == null) {
+			IndexReader reader = IndexReader.open(directory);
+			searcher = new IndexSearcher(reader);
+		}
+		
+		return searcher;
 	}
 	
 	private void cleanupFragments(String[] fragments) {
@@ -318,7 +320,7 @@ public class PageIndex {
 			fragments[i] = fragments[i].replaceAll("^[,\\.]+", StringUtils.EMPTY).trim(); //$NON-NLS-1$
 		}
 	}
-
+	
 	public int getNumDocuments() throws IOException {
 		Directory directory = null;
 		IndexReader reader = null;
