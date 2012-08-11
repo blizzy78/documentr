40bd6c17f790a973f41d5da396c4526d6aacae69
Maik Schreiber <blizzy@blizzy.de>
2012-08-11 13:08:18 +0200

    add more unit tests

--- de.blizzy.documentr/src/test/java/de/blizzy/documentr/web/access/AccessControllerTest.java
+++ de.blizzy.documentr/src/test/java/de/blizzy/documentr/web/access/AccessControllerTest.java
@@ -18,9 +18,16 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 package de.blizzy.documentr.web.access;
 
 import static org.junit.Assert.*;
+import static org.mockito.Mockito.*;
 
-import org.junit.Ignore;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
+
 import org.junit.Test;
+import org.springframework.security.access.AccessDeniedException;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.web.WebAttributes;
+import org.springframework.ui.Model;
 
 public class AccessControllerTest {
 	@Test
@@ -29,14 +36,30 @@ public class AccessControllerTest {
 	}
 
 	@Test
-	@Ignore
 	public void loginError() {
-		// TODO: implement test
+		AuthenticationException ex = mock(AuthenticationException.class);
+		when(ex.getMessage()).thenReturn("message"); //$NON-NLS-1$
+		HttpSession session = mock(HttpSession.class);
+		when(session.getAttribute(WebAttributes.AUTHENTICATION_EXCEPTION)).thenReturn(ex);
+		Model model = mock(Model.class);
+		
+		String view = new AccessController().loginError(session, model);
+		assertEquals("/login", view); //$NON-NLS-1$
+		
+		verify(model).addAttribute("errorMessage", "message"); //$NON-NLS-1$ //$NON-NLS-2$
 	}
 
 	@Test
-	@Ignore
 	public void loginForbidden() {
-		// TODO: implement test
+		AccessDeniedException ex = mock(AccessDeniedException.class);
+		when(ex.getMessage()).thenReturn("message"); //$NON-NLS-1$
+		HttpServletRequest request = mock(HttpServletRequest.class);
+		when(request.getAttribute(WebAttributes.ACCESS_DENIED_403)).thenReturn(ex);
+		Model model = mock(Model.class);
+		
+		String view = new AccessController().loginForbidden(request, model);
+		assertEquals("/login", view); //$NON-NLS-1$
+		
+		verify(model).addAttribute("errorMessage", "message"); //$NON-NLS-1$ //$NON-NLS-2$
 	}
 }
