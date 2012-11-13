19dd26dfc1b8b59f968385b74a2bb234e4545b87
Maik Schreiber <blizzy@blizzy.de>
2012-11-13 14:31:53 +0100

    add new {{if}} macro

---
+++ de.blizzy.documentr/src/main/java/de/blizzy/documentr/markdown/macro/impl/IfMacro.java
@@ -0,0 +1,72 @@
+/*
+documentr - Edit, maintain, and present software documentation on the web.
+Copyright (C) 2012 Maik Schreiber
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+package de.blizzy.documentr.markdown.macro.impl;
+
+import groovy.lang.Binding;
+import groovy.lang.GroovyShell;
+import lombok.extern.slf4j.Slf4j;
+
+import org.apache.commons.lang3.StringUtils;
+import org.codehaus.groovy.control.CompilerConfiguration;
+import org.codehaus.groovy.control.customizers.ImportCustomizer;
+
+import de.blizzy.documentr.DocumentrConstants;
+import de.blizzy.documentr.markdown.HtmlSerializerContext;
+import de.blizzy.documentr.markdown.macro.IMacroContext;
+import de.blizzy.documentr.markdown.macro.IMacroRunnable;
+import de.blizzy.documentr.markdown.macro.Macro;
+
+@Macro(name="if", insertText="{{if EXPRESSION}}[CONTENTS]{{/if}}", cacheable=false)
+@Slf4j
+public class IfMacro implements IMacroRunnable {
+	@Override
+	public String getHtml(IMacroContext macroContext) {
+		String code = macroContext.getParameters();
+		if (StringUtils.isNotBlank(code)) {
+			try {
+				GroovyShell shell = getGroovyShell(macroContext);
+				log.debug("evaluating expression: {}", code); //$NON-NLS-1$
+				Object result = shell.evaluate(code);
+				if ((result instanceof Boolean) && ((Boolean) result).booleanValue()) {
+					return macroContext.getBody();
+				}
+			} catch (RuntimeException e) {
+				log.error("error evaluating {{if}} expression", e); //$NON-NLS-1$
+			}
+		}
+		return null;
+	}
+
+	private GroovyShell getGroovyShell(IMacroContext macroContext) {
+		HtmlSerializerContext htmlSerializerContext = macroContext.getHtmlSerializerContext();
+		Binding binding = new Binding();
+		binding.setVariable("projectName", htmlSerializerContext.getProjectName()); //$NON-NLS-1$
+		binding.setVariable("branchName", htmlSerializerContext.getBranchName()); //$NON-NLS-1$
+		binding.setVariable("pagePath", htmlSerializerContext.getPagePath()); //$NON-NLS-1$
+		ImportCustomizer importCustomizer = new ImportCustomizer();
+		importCustomizer.addStarImports(DocumentrConstants.GROOVY_DEFAULT_IMPORTS);
+		CompilerConfiguration compilerConfiguration = new CompilerConfiguration();
+		compilerConfiguration.addCompilationCustomizers(importCustomizer);
+		return new GroovyShell(binding, compilerConfiguration);
+	}
+
+	@Override
+	public String cleanupHtml(String html) {
+		return null;
+	}
+}
---
+++ de.blizzy.documentr/src/test/java/de/blizzy/documentr/markdown/macro/impl/IfMacroTest.java
@@ -0,0 +1,62 @@
+/*
+documentr - Edit, maintain, and present software documentation on the web.
+Copyright (C) 2012 Maik Schreiber
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+package de.blizzy.documentr.markdown.macro.impl;
+
+import static org.junit.Assert.*;
+import static org.mockito.Mockito.*;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+
+import de.blizzy.documentr.AbstractDocumentrTest;
+import de.blizzy.documentr.markdown.HtmlSerializerContext;
+import de.blizzy.documentr.markdown.macro.IMacroContext;
+
+public class IfMacroTest extends AbstractDocumentrTest {
+	private static final String EXPRESSION = "branchName == 'branch'"; //$NON-NLS-1$
+	private static final String BODY = "body"; //$NON-NLS-1$
+	private static final String PROJECT = "project"; //$NON-NLS-1$
+	private static final String BRANCH = "branch"; //$NON-NLS-1$
+	private static final String PAGE = "page"; //$NON-NLS-1$
+	
+	@Mock
+	private IMacroContext context;
+	@Mock
+	private HtmlSerializerContext htmlSerializerContext;
+	@InjectMocks
+	private IfMacro macro;
+
+	@Before
+	public void setUp() {
+		when(context.getParameters()).thenReturn(EXPRESSION);
+		when(context.getBody()).thenReturn(BODY);
+		when(context.getHtmlSerializerContext()).thenReturn(htmlSerializerContext);
+		
+		when(htmlSerializerContext.getProjectName()).thenReturn(PROJECT);
+		when(htmlSerializerContext.getBranchName()).thenReturn(BRANCH);
+		when(htmlSerializerContext.getPagePath()).thenReturn(PAGE);
+	}
+	
+	@Test
+	public void getHtml() {
+		String html = macro.getHtml(context);
+		assertEquals(BODY, html);
+	}
+}
